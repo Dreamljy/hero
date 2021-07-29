ad13eef0fb348b7f1b91ee8fbe88828a27e2862f
‘lin1134’ <1134809127@qq.com>
2021-07-29 10:27:32 +0800

    code style

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,22 +1,27 @@
+import { Layout, Menu } from 'antd';
 import React from 'react';
+import { Link } from 'alita';
 import styles from './index.less';
 
-//使用antd框架
-import { Layout,Menu } from 'antd';
 const { Header, Content, Footer } = Layout;
-//alita路由,里面封装了react-route的路由
-import { Link } from 'alita';
+
 const BasicLayout: React.FC = ({ children }) => {
-    return(
+
+    let b = 1;
+    const c = 2;
+
+    const a = () => { }
+
+    return (
         <Layout>
             <Header>
                 <div className={styles.logo}>王者荣耀资料库</div>
-                <Menu theme="dark" 
-                    mode="horizontal" 
+                <Menu theme="dark"
+                    mode="horizontal"
                 >
                     {
-                        menuData.map((item,index)=>{
-                            return(
+                        menuData.map((item, index) => {
+                            return (
                                 <Menu.Item key={index}  >
                                     <Link to={item.route}>{item.name}</Link>
                                 </Menu.Item>
@@ -26,14 +31,14 @@ const BasicLayout: React.FC = ({ children }) => {
 
                 </Menu>
             </Header>
-            
+
             <Content style={{ padding: '0 50px' }}>
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-                {children}
-            </div>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                    {children}
+                </div>
             </Content>
             <Footer style={{ textAlign: 'center' }}>
-              Alita 入门教程 Created by xiaohuoni
+                Alita 入门教程 Created by xiaohuoni
             </Footer>
         </Layout>
     )
@@ -41,7 +46,7 @@ const BasicLayout: React.FC = ({ children }) => {
 };
 
 
-const menuData =[
+const menuData = [
     { route: 'hero', name: '英雄' },
     { route: 'item', name: '局内道具' },
     { route: 'summoner', name: '召唤师技能' },
